3318e525cfd07793e35d8fe4fd09c431c8ff99dd
Johann Muszynski <johann.muszynski@gmail.com>
2017-06-12 22:09:00 +0300

    Add SDL binaries and headers

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,5 @@
 project_name = "voxed"
+ext_path = "ext/"
 
 workspace (project_name)
     configurations { "debug", "release" }
@@ -11,6 +12,9 @@ workspace (project_name)
     symbols ("On")
     targetdir ("bin/%{cfg.system}/%{cfg.buildcfg}")
 
+    filter "action:vs*"
+        defines { "_CRT_SECURE_NO_WARNINGS" }
+
 project (project_name)
     kind ("ConsoleApp")
     warnings ("Extra")
@@ -18,8 +22,20 @@ project (project_name)
         "src/**.cpp",
         "src/**.h",
     }
+
     filter "configurations:debug"
         defines { "DEBUG" }
     filter "configurations:release"
         defines { "NDEBUG" }
         optimize ("Speed")
+
+    filter "system:macosx"
+        includedirs { "/Library/Frameworks/SDL2.framework/Headers" }
+        links { "Cocoa.framework" }
+        linkoptions { "-F/Library/Frameworks -framework SDL2" }
+
+    filter "system:windows"
+        includedirs { ext_path.."SDL-2.0.4/include" }
+        libdirs { ext_path.."SDL-2.0.4/bin/win64" }
+        links { "SDL2", "SDL2main", "opengl32" }
+        postbuildcommands { "{COPY} ".. ext_path.."SDL-2.0.4/bin/win64/SDL2.dll %{cfg.targetdir}" }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
+#include "SDL.h"
 #include <stdio.h>
 
 int main(int /*argc*/, char** /*argv*/)
 {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
     printf("Hello, voxed!\n");
+
+    SDL_Quit();
+
+    return 0;
 }
