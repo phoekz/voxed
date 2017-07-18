2890211dcb5b873c19ae5e7abb727e1faaea36e1
Johann Muszynski <johann.muszynski@gmail.com>
2017-07-18 23:07:07 +0300

    Show only one quadrant of grid rulers

--- src/common/intersection.h
+++ src/common/intersection.h
@@ -1,55 +1,10 @@
 #pragma once
 
 #include "geometry.h"
-#include "gtx/norm.hpp"
-
-#include <algorithm>
-#include <cassert>
-#include <limits>
 
 namespace vx
 {
-static bool ray_intersects_aabb(const ray& r, const bounds3f& b, float* out_t)
-{
-    float inv_dx = 1.f / r.direction.x;
-    float tmin = (b.min.x - r.origin.x) * inv_dx;
-    float tmax = (b.max.x - r.origin.x) * inv_dx;
+bool ray_intersects_aabb(const ray& r, const bounds3f& b, float* out_t);
 
-    float inv_dy = 1.f / r.direction.y;
-    float tymin = (b.min.y - r.origin.y) * inv_dy;
-    float tymax = (b.max.y - r.origin.y) * inv_dy;
-
-    if (tmin > tmax)
-        std::swap(tmin, tmax);
-
-    if (tymin > tymax)
-        std::swap(tymin, tymax);
-
-    if ((tmin > tymax) || (tymin > tmax))
-        return false;
-
-    if (tymin > tmin)
-        tmin = tymin;
-
-    if (tymax < tmax)
-        tmax = tymax;
-
-    float inv_dz = 1.f / r.direction.z;
-    float tzmin = (b.min.z - r.origin.z) * inv_dz;
-    float tzmax = (b.max.z - r.origin.z) * inv_dz;
-
-    if (tzmin > tzmax)
-        std::swap(tzmin, tzmax);
-
-    if ((tmin > tzmax) || (tzmin > tmax))
-        return false;
-
-    if (tzmin > tmin)
-        tmin = tzmin;
-
-    if (out_t)
-        *out_t = tmin;
-
-    return true;
-}
+void ray_intersects_plane(const ray& r, const plane& plane, float& out_t);
 }
--- src/voxed.h
+++ src/voxed.h
@@ -8,9 +8,17 @@ namespace vx
 {
 struct voxed_state;
 
+enum class voxed_mode
+{
+    voxel,
+    box
+};
+
 voxed_state* voxed_create();
 
-void voxed_update(const platform& platform, voxed_state* state, float dt);
+void voxed_set_mode(voxed_state* state, voxed_mode mode);
+
+void voxed_update(voxed_state* state, const platform& platform, float dt);
 
 void voxed_render(voxed_state* state);
 
