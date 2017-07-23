13092e2ac7c0c969ed99c8afcd055b72d04a780c
Vinh Truong <vinhphuc.truong@gmail.com>
2017-07-23 23:02:17 +0300

    Add stringify macros

--- src/common/macros.h
+++ src/common/macros.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #define vx_countof(arr) (int)(sizeof(arr) / sizeof(arr[0]))
+#define vx_xstr(s) vx_str(s)
+#define vx_str(s) #s
 
 #define KB *(1024ull)
 #define MB *(1024ull * 1024ull)
